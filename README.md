48f4ba2f8e268b0556bab42e2116ec9573cf916b
Whitney Champion <whitney.ellis.champion@gmail.com>
2020-09-02 16:57:49 -0400

    add all the things!

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # logs_to_slack
+
+Quick and dirty python script/systemd service that will watch a log file and spit out some or all contents to Slack via webhook.
+
+This assumes your logs are JSON. If they are not, you can always do something similar to the following in `logs_to_slack.py`:
+
+```
+line = line.split(" - ")
+
+field1 = line[0]
+field2 = line[1]
+field3 = line[2]
+```
+
+## Install 
+
+```
+pip install -r requirements.txt 
+cp logs_to_slack.service /etc/systemd/system/
+cp logs_to_slack.py /opt
+systemctl enable logs_to_slack 
+systemctl start logs_to_slack
+```
---
+++ logs_to_slack.py
@@ -0,0 +1,63 @@
+import os
+import sys
+import requests
+import time
+import json
+
+def follow(thefile):
+    thefile.seek(0,2)
+    while True:
+        line = thefile.readline()
+        if not line:
+            time.sleep(0.1)
+            continue
+        yield line
+
+def logs_to_slack():
+
+    logfile = open('/Users/shortstack/Downloads/test.json', 'r')
+    loglines = follow(logfile)
+
+    for line in loglines: 
+
+        if "unicorn" in line:           # you can parse for specific entries by keyword, or comment this out to include everything  
+    
+            line = json.loads(line) 
+
+            field1 = line["field1"]
+            field2 = line["field2"]
+            field3 = line["field3"]
+
+            webhook_url = "https://hooks.slack.com/goes/here"
+            data = {
+                "attachments": [
+                    {
+                        "fallback": line,
+                        "icon_emoji": ":unicorn:",
+                        "color": "#FF1694",
+                        "title": "My Logs",
+                        "title_link": "https://angry.unicorns.lol",
+                        "fields": [
+                            {
+                                "title": "Field 1",
+                                "value": field1,
+                                "short": False
+                            },
+                            {
+                                "title": "Field 1",
+                                "value": field2,
+                                "short": False
+                            },
+                            {
+                                "title": "Field 1",
+                                "value": field3,
+                                "short": False
+                            }
+                        ]
+                    }
+                ]
+            }
+
+            response = requests.post(webhook_url, data=json.dumps(data), headers={'Content-Type': 'application/json'})
+
+logs_to_slack()
